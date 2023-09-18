306739cebc06da51f85e1410b9fd50d826a5458e
André Holton <142992144+Yeahger1@users.noreply.github.com>
2023-09-18 12:52:56 +0200

    Fixed tablepassing

--- Assets/Scripts/Gubbe.cs
+++ Assets/Scripts/Gubbe.cs
@@ -6,10 +6,10 @@ public class BarMan : MonoBehaviour
 {
     public Rigidbody2D Barman;
     Rigidbody2D Gubben;
-    public float heroSpeed = 0.001f;
-    Vector2 position = Vector2.zero;
+    public float heroSpeed = 5f;
+   
     Vector2 rawInput;
-    Vector2 velocity;
+    Vector3 velocity;
 
 
     // Start is called before the first frame update
@@ -21,6 +21,8 @@ public class BarMan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
         rawInput.x = Input.GetAxisRaw("Horizontal");
         rawInput.y = Input.GetAxisRaw("Vertical");
         if (rawInput.sqrMagnitude > 1)
@@ -29,8 +31,7 @@ public class BarMan : MonoBehaviour
         }
 
         velocity = rawInput * (heroSpeed * Time.deltaTime);
-        position += velocity;
+        transform.position += velocity;
 
-        transform.position = position;
     }
 }
