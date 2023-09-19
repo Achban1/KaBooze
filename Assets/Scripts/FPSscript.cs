6d64982f8c778ae4375bfff79e49dc632ccee106
Oscar Westerdahl <oscar.westerdahl@hotmail.com>
2023-09-19 17:44:00 +0200

    Added  Glasses

--- Assets/Scripts/FPSscript.cs
+++ Assets/Scripts/FPSscript.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class FPSscript : MonoBehaviour
 {
+    public Transform cameraTransform;
+    public float shakeDuration = 0.3f;
+    public float shakeMagnitude = 0.2f;
+
+    private Vector3 originalPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
+        if (cameraTransform == null )
+            cameraTransform = GetComponent<Transform>();
+    }
+    public void Shake()
+    {
+        originalPosition = cameraTransform.localPosition;
+        InvokeRepeating("StartShaking", 0f, 0.01f);
+        Invoke("StopShaking", shakeDuration);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartShaking()
     {
-        
+        float xOffset = Random.Range(-1f, 1f) * shakeMagnitude;
+        float yOffest = Random.Range(-1f, 1f) * shakeMagnitude;
+
+        Vector3 pos = originalPosition + new Vector3(xOffset, yOffest, 0f);
+        cameraTransform.localPosition = pos;
     }
+
+    private void StopShaking ()
+    {
+        CancelInvoke("StartShaking");
+        cameraTransform.localPosition = originalPosition;
+    }
+
+
+
 }
