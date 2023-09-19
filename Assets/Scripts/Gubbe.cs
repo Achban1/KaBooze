2a8d8aa32cda1da449f3d70e3ce4ef9441766280
André Holton <142992144+Yeahger1@users.noreply.github.com>
2023-09-19 11:37:24 +0200

    Broken Damadge

--- Assets/Scripts/Gubbe.cs
+++ Assets/Scripts/Gubbe.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody2D Barman;
-    
+    public PlayerHealthScript health;
+
     public float heroSpeed = 5f;
    
     Vector2 rawInput;
@@ -15,7 +17,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = GameObject.FindGameObjectWithTag("Health").GetComponent<PlayerHealthScript>();
     }
 
     // Update is called once per frame
@@ -31,5 +33,13 @@ public class Player : MonoBehaviour
 
         velocity = rawInput * (heroSpeed * Time.deltaTime);
         transform.position += velocity;
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            Debug.Log("Damage Taken");
+            health.PlayerDamage();
+        }
     }
 }
