24a32f9df93f2cdf009e61bd64c30cb4dabe5f4a
André Holton <142992144+Yeahger1@users.noreply.github.com>
2023-09-20 11:53:05 +0200

    Animation works for mob

--- Assets/Scripts/AngryBumScript.cs
+++ Assets/Scripts/AngryBumScript.cs
@@ -10,7 +10,13 @@ public class AngryBumScript : MonoBehaviour
     //public GameObject AngryBum;
     public SpriteRenderer spriteRenderer;
     //public Sprite[] spriteList;  
-
+    public float lastX = 0;
+    public float currentX = 0;
+    public float movingX = 0;
+    public float lastY = 0;
+    public float currentY = 0;
+    public float movingY = 0;
+    public Animator animator;
     Transform target;
     Rigidbody2D rb2D;
 
@@ -27,15 +33,54 @@ public class AngryBumScript : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-
+    {           
+        //currentX = transform.position.x;
+        //currentY = transform.position.y;        
+                
+        //movingY = currentY - lastY;
+        //movingX = currentX - lastX;
+        
+        //if (Mathf.Abs(movingX) > Mathf.Abs(movingY))
+        //{
+        //    animator.SetFloat("movingX", movingX);
+        //    animator.SetFloat("movingY", 0);
+        //}
+        //else
+        //{
+        //    animator.SetFloat("movingY", movingY);
+        //    animator.SetFloat("movingX", 0);
+        //}
+        
+        //lastY = currentY;
+        //lastX = currentX;
         Vector2 direction = target.position - transform.position;
+
+
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            animator.SetFloat("movingX", direction.x);
+            animator.SetFloat("movingY", 0);
+        }
+        else
+        {
+            animator.SetFloat("movingY", direction.y);
+            animator.SetFloat("movingX", 0);
+        }
+        if (direction.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX= false;
+        }
+
 
         direction.Normalize();
 
         rb2D.velocity = direction * speed;
 
-        transform.right = direction;
+        //transform.right = direction;
 
         if (bumHealth <= 0)
         {
