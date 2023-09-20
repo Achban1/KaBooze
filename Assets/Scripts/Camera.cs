a7f594f2175faa07d51953e8e2ff91b48c395bbb
Oscar Westerdahl <oscar.westerdahl@hotmail.com>
2023-09-20 09:22:36 +0200

    Update Camera.cs

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FPSscript : MonoBehaviour
+public class Camera : MonoBehaviour
 {
     public Transform cameraTransform;
     public float shakeDuration = 0.3f;
