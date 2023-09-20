8a999d82802571c2cb4089b6d23dfb847a33132f
Oscar Westerdahl <oscar.westerdahl@hotmail.com>
2023-09-20 09:13:02 +0200

    Changed names on FPSScript to CameraScript

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraScript : MonoBehaviour
+public class FPSscript : MonoBehaviour
 {
     public Transform cameraTransform;
     public float shakeDuration = 0.3f;
