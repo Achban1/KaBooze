79ceb56952efe78407d930aa12177aef018314d7
André Holton <142992144+Yeahger1@users.noreply.github.com>
2023-09-21 15:19:35 +0200

    Update TableScript.cs

--- Assets/Scripts/TableScript.cs
+++ Assets/Scripts/TableScript.cs
@@ -7,7 +7,7 @@ public class TableScript : MonoBehaviour
 {
     public PlayerHealthScript health;
     public GameObject []AngryBum;
-    public float bumCount =1;
+    public float bumCount = 1;
     public float bumTimer;
     public float cash = 0;
     public CoinCounterScript CoinCounterScript;
@@ -22,7 +22,7 @@ public class TableScript : MonoBehaviour
         CoinCounterScript = GameObject.FindGameObjectWithTag("CoinCounterCanvas").GetComponent<CoinCounterScript>();
         health = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthScript>();
         cash = 1;
-        tablePos = new Vector3 (transform.position.x, transform.position.y - 0.2f, 0);
+        tablePos = new Vector3 (transform.position.x, transform.position.y , 0);
     }
 
     // Update is called once per frame
@@ -35,14 +35,14 @@ public class TableScript : MonoBehaviour
         else if (bumCount < 4) 
         {
             //Setting bumPosY to avoid bump while bumPosX is 0
-            //if (bumPosX == 0) 
-            //{ 
-            //    bumPosY = -0.3f;
-            //}
-            //else
-            //{
-            //    bumPosY = 0;
-            //}
+            if (bumPosX == 0)
+            {
+                bumPosY = -0.3f;
+            }
+            else
+            {
+                bumPosY = 0;
+            }
             bumSpawn();
             bumPosX += 0.5f;
             bumCount++;
@@ -52,6 +52,28 @@ public class TableScript : MonoBehaviour
         {
             cash += Random.Range(2, 8);
         }
+        switch (bumCount)
+        {
+            case 1:
+                {
+                    tablePos = transform.position;
+                    tablePos = new Vector3(tablePos.x + 0.246f, tablePos.y + -0.034f, 0);
+                    break;
+                }
+            case 2:
+                {
+                    tablePos = transform.position;
+                    tablePos = new Vector3(tablePos.x - 0.013f, tablePos.y - 0.207f, 0);
+                    break;
+                }
+            case 3:
+                {
+                    tablePos = transform.position;
+                    tablePos = new Vector3(tablePos.x - 0.299f, tablePos.y  -0.14f, 0);
+                    break;
+                }
+        }
+        
         //if (bumCount < 5)
         //{
         //    int randomMob = Random.Range(0, 5);
@@ -66,21 +88,8 @@ public class TableScript : MonoBehaviour
         int randomMob = Random.Range(0, 5);
         GameObject selectedMob = AngryBum[randomMob];
         GameObject newMob = Instantiate(selectedMob, new Vector3(0, -4.5f, 0), transform.rotation);
-        //Ändra här
-        if (bumCount == 1)
-        {
-            newMob.GetComponent<AngryBumScript>().myTable = new Vector3(tablePos.x + 0.031f, tablePos.y + -0.192f, 0);
-        }
-        else if(bumCount == 2)
-        {
-            newMob.GetComponent<AngryBumScript>().myTable = new Vector3(tablePos.x + -0.268f, tablePos.y + -0.125f, 0);
-        }
-        else if (bumCount == 3)
-        {
-            newMob.GetComponent<AngryBumScript>().myTable = new Vector3(tablePos.x + 0.232f, tablePos.y + -0.058f, 0);
-        }
+        newMob.GetComponent<AngryBumScript>().myTable = tablePos;
     }
-
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
