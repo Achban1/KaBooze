59a048a4fa243f56384d4823f0270b747b9fe8e0
André Holton <142992144+Yeahger1@users.noreply.github.com>
2023-09-21 21:53:28 +0200

    flyttat stolar

--- Assets/Scripts/AngryBumScript.cs
+++ Assets/Scripts/AngryBumScript.cs
@@ -37,7 +37,7 @@ public class AngryBumScript : MonoBehaviour
     void Update()
     {
         tableBumped = tableScript.tableBumped;
-        if (timer > 3 || myTable.sqrMagnitude - transform.position.sqrMagnitude < 1)
+        if (timer > 3 || myTable.sqrMagnitude - transform.position.sqrMagnitude < 0.9f)
         {
             boxC.enabled = true;
             speed = 0;
