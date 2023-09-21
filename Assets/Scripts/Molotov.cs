a4d4d0e40770cf2e611d7fe8ac2d1709180ab173
Erik Forssell <Erikeforssell@gmail.com>
2023-09-21 16:08:04 +0200

    Added explosion/ changed molotov script p1

--- Assets/Scripts/Molotov.cs
+++ Assets/Scripts/Molotov.cs
@@ -14,17 +14,20 @@ public class Molotov : MonoBehaviour
     public Transform playerPos;
     public bool rageMode;
     public GameObject GlassArea;
+    public GameObject Explosion;
+
     public bartenderThrow Mode;
     private float rotationSpeed;
-    public AudioSourcePoolBottles audioSourcePoolBottles;
+    public AudioScriptGlassBottle audioScriptGlassBottle;
     public Sprite typeOfExplosion;
 
     void Start()
     {
+
         playerPos = GameObject.FindGameObjectWithTag("Player").transform;
         rb20 = GetComponent<Rigidbody2D>();
         Mode = GameObject.FindGameObjectWithTag("Bartender").GetComponent<bartenderThrow>();
-        audioSourcePoolBottles = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSourcePoolBottles>();
+        audioScriptGlassBottle = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioScriptGlassBottle>();
         
         
 
@@ -66,7 +69,10 @@ public class Molotov : MonoBehaviour
         if (Vector2.Distance(transform.position, destination) < 0.1f)
         {           
             Destroy(gameObject);
-            Instantiate(GlassArea, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+            Instantiate(GlassArea, new Vector3(transform.position.x, transform.position.y, 1), Quaternion.identity);
+            GameObject newExplosion  = Instantiate(Explosion, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+            Destroy(newExplosion, 0.4f);
+            
         }
     }
 
@@ -90,7 +96,7 @@ public class Molotov : MonoBehaviour
         if (col.gameObject.CompareTag("Player") || col.gameObject.layer == 7 || col.gameObject.layer == 6)
         {
 
-            audioSourcePoolBottles.BottleSoundFX();
+            audioScriptGlassBottle.GlassBreakSound();
             Destroy(gameObject);
             
         }
