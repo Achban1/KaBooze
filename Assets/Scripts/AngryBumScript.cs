ca39845be1b848b5334f129f7ec27cee2be396c1
André Holton <142992144+Yeahger1@users.noreply.github.com>
2023-09-25 15:42:34 +0200

    Cleaned up AngryBumScript and TableScripts

--- Assets/Scripts/AngryBumScript.cs
+++ Assets/Scripts/AngryBumScript.cs
@@ -6,59 +6,27 @@ using UnityEngine;
 public class AngryBumScript : MonoBehaviour
 {
     public TableScript tableScript;
-    public float speed = 3;
-    public float movingX = 0;
-    public float movingY = 0;
-    public bool fall;
     public Animator animator;
     public SpriteRenderer spriteRenderer;
     Transform target;
     Rigidbody2D rb2D;
     BoxCollider2D boxC;
-    public float timer; 
-    public int tableBumped;
-    public bool heTookMyTip = false;
-    public int tableNr;
-    public Vector3 myTable;
-    
 
+    public float speed = 3;    
+    public bool fall;
+    public float timer;     
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
         tableScript = GameObject.FindGameObjectWithTag("Furniture").GetComponent<TableScript>();
-        rb2D = GetComponent<Rigidbody2D>();
-        //boxC = GetComponent<BoxCollider2D>();
-        //boxC.enabled = false;
+        rb2D = GetComponent<Rigidbody2D>();        
     }
-    //
+    
     // Update is called once per frame
     void Update()
-    {
-
-        //tableBumped = tableScript.tableBumped;
-        //if (timer > 3 || myTable.sqrMagnitude - transform.position.sqrMagnitude < 0.8f)
-        //{
-        //    boxC.enabled = true;
-        //    speed = 0;
-        //    animator.SetBool("still", true);
-        //}
-        //if (tableBumped == tableNr)
-        //{
-        //    heTookMyTip = true;
-        //}
-        //if (heTookMyTip == true)
-        //{
-        //    direction = target.position - transform.position;
-        //    speed = 2;
-        //    animator.SetBool("still", false);
-        //}
-        //else
-        //{
-
-        //}
-
+    {       
         Vector2 direction = target.position - transform.position;
         direction.Normalize();
         rb2D.velocity = direction * speed;
@@ -85,10 +53,8 @@ public class AngryBumScript : MonoBehaviour
         if (fall == true)
         {
             speed = 0;
-        }
-        
+        }        
 
-        //timer += Time.deltaTime;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
--- Assets/Scripts/TableScript.cs
+++ Assets/Scripts/TableScript.cs
@@ -7,31 +7,27 @@ public class TableScript : MonoBehaviour
 {
     public CoinCounterScript CoinCounterScript;
     public PlayerHealthScript health;
-    public GameObject[] AngryBum;
-    public AngryBumScript AngryBumScript;
+    public GameObject[] AngryBum;    
     public Player player;
+
     public int bumCount = 1;
     public float bumTimer;
-    public int cash = 0;
-    public float ThisTableNr = 0;
-    public float tableBumped = 0;
+    public int cash = 0;    
+
     float stepCat = 0;
     float oneTime = 0;
+
     Vector3 chair1;
     Vector3 chair2;
     Vector3 chair3;
-    int randomMob;
-
-    public Vector3 tablePos = Vector3.zero;
+    int randomMob;    
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         CoinCounterScript = GameObject.FindGameObjectWithTag("CoinCounterCanvas").GetComponent<CoinCounterScript>();
-        health = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthScript>();
-        cash = 1;
-        tablePos = new Vector3(transform.position.x, transform.position.y, 0);
+        health = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthScript>();    
         chair1 = this.transform.GetChild(0).transform.position;
         chair2 = this.transform.GetChild(1).transform.position;
         chair3 = this.transform.GetChild(2).transform.position;
@@ -62,42 +58,13 @@ public class TableScript : MonoBehaviour
         {
             bumCount++;
             bumTimer = 0;
-        }
-        //else if (bumCount < 4)
-        //{
-        //    bumSpawn();
-        //    bumCount++;
-        //    bumTimer = 0;
-        //}        
-        
+        }        
 
-        //if (bumCount < 5)
-        //{
-        //    int randomMob = Random.Range(0, 5);
-        //    GameObject selectedMob = AngryBum[randomMob];
-        //    Instantiate(selectedMob, new Vector3(0, -4.5f, 0), transform.rotation);
-        //    bumCount++;
-        //}
-
-    }
-    //private void bumSpawn()
-    //{
-    //    int randomMob = Random.Range(0, 5);
-    //    GameObject selectedMob = AngryBum[randomMob];
-    //    GameObject newMob = Instantiate(selectedMob, new Vector3(0, -4.5f, 0), transform.rotation);
-    //    newMob.GetComponent<AngryBumScript>().myTable = tablePos;
-    //    //Setting tablenumber for bumping Table
-    //    newMob.GetComponent<AngryBumScript>().tableNr = ThisTableNr;
-    //}
+    }    
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Change to playerLayer
-        //if (collision.gameObject.layer == 3) //&& bumCount < 2) 
-        //{            
-        //    Debug.Log("CoinCollected");
-        //    CoinCounterScript.CollectCoin();
-        //}
+        
         if (collision.gameObject.layer == 3 && bumCount != 0)
         {
             if (bumCount == 1)
@@ -130,57 +97,16 @@ public class TableScript : MonoBehaviour
                 randomMob = Random.Range(0, 5);
                 GameObject selectedMob3 = AngryBum[randomMob];
                 GameObject newMob3 = Instantiate(selectedMob3, chair3, transform.rotation);                    
-            }
+            }            
             
-            //
-            //GameObject selectedMob = AngryBum[randomMob];
-            //GameObject newMob = Instantiate(selectedMob, tablePos, transform.rotation);
-            //tableBumped = ThisTableNr;
             health.collectedCash = health.collectedCash + cash;
             Debug.Log("CoinCollected");
-            CoinCounterScript.CollectCoin(cash);
-            //Telling AngryBums that someone stole their tip
+            CoinCounterScript.CollectCoin(cash);            
             bumCount = 0;
             cash = 0;
 
         }
 
-    }
-    public void spamBum()
-    {        
-        switch (bumCount)
-        {
-            case 1:
-                {
-                    randomMob = Random.Range(0, 5);
-                    GameObject selectedMob = AngryBum[randomMob];
-                    GameObject newMob = Instantiate(selectedMob, chair1, transform.rotation);
-                    break;
-                }
-            case 2:
-                {
-                    randomMob = Random.Range(0, 5);
-                    GameObject selectedMob = AngryBum[randomMob];
-                    GameObject newMob = Instantiate(selectedMob, chair1, transform.rotation);
-                    randomMob = Random.Range(0, 5);
-                    GameObject selectedMob2 = AngryBum[randomMob];
-                    GameObject newMob2 = Instantiate(selectedMob2, chair2, transform.rotation);
-                    break;
-                }
-            case 3:
-                {
-                    randomMob = Random.Range(0, 5);
-                    GameObject selectedMob = AngryBum[randomMob];
-                    GameObject newMob = Instantiate(selectedMob, chair1, transform.rotation);
-                    randomMob = Random.Range(0, 5);
-                    GameObject selectedMob2 = AngryBum[randomMob];
-                    GameObject newMob2 = Instantiate(selectedMob2, chair2, transform.rotation);
-                    randomMob = Random.Range(0, 5);
-                    GameObject selectedMob3 = AngryBum[randomMob];
-                    GameObject newMob3 = Instantiate(selectedMob3, chair3, transform.rotation);
-                    break;
-                }
-        }
-    }
+    }   
 
 }
