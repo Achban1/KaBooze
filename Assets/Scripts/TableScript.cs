5a39994a603e109f0de74aba458ebb77f1b3fa44
André Holton <142992144+Yeahger1@users.noreply.github.com>
2023-09-26 13:11:06 +0200

    Update TableScript.cs
    large coinstack gives 7 coins now

--- Assets/Scripts/TableScript.cs
+++ Assets/Scripts/TableScript.cs
@@ -14,12 +14,9 @@ public class TableScript : MonoBehaviour
     public float bumTimer;
     public int cash = 0;    
 
-    float stepCat = 0;
+    float stepOnCat = 0;
     float oneTime = 0;
-
-    Vector3 chair1;
-    Vector3 chair2;
-    Vector3 chair3;
+       
     int randomMob;    
 
     // Start is called before the first frame update
@@ -27,86 +24,54 @@ public class TableScript : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         CoinCounterScript = GameObject.FindGameObjectWithTag("CoinCounterCanvas").GetComponent<CoinCounterScript>();
-        health = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthScript>();    
-        chair1 = this.transform.GetChild(0).transform.position;
-        chair2 = this.transform.GetChild(1).transform.position;
-        chair3 = this.transform.GetChild(2).transform.position;
+        health = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthScript>();           
     }
 
     // Update is called once per frame
     void Update()
     {
-        stepCat = player.StepOnCat;
-        if (stepCat == 1 && oneTime == 0)
+        stepOnCat = player.StepOnCat;
+        if (stepOnCat == 1 && oneTime == 0)
         {
-            randomMob = Random.Range(0, 5);
-            GameObject selectedMob = AngryBum[randomMob];
-            GameObject newMob = Instantiate(selectedMob, chair1, transform.rotation);
-            randomMob = Random.Range(0, 5);
-            GameObject selectedMob2 = AngryBum[randomMob];
-            GameObject newMob2 = Instantiate(selectedMob2, chair2, transform.rotation);
-            randomMob = Random.Range(0, 5);
-            GameObject selectedMob3 = AngryBum[randomMob];
-            GameObject newMob3 = Instantiate(selectedMob3, chair3, transform.rotation);
+            for (int i = 0; i < 3; i++)
+            {
+                randomMob = Random.Range(0, 5);
+                GameObject selectedMob = AngryBum[randomMob];
+                GameObject newMob = Instantiate(selectedMob, this.transform.GetChild(i).transform.position, transform.rotation);
+            }            
             oneTime = 1;
         }
-        if (bumTimer < 10 && bumCount <= 3)
+        if (bumTimer < 10 && bumCount < 3)
         {
-            bumTimer += Time.deltaTime;
+            bumTimer += Time.deltaTime;            
         }
         else
         {
             bumCount++;
-            bumTimer = 0;
+            bumTimer = 0;            
         }        
-
+        if (bumCount > 3)
+        {
+            bumCount = 3;            
+        }
     }    
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        
+    {        
         if (collision.gameObject.layer == 3 && bumCount != 0)
         {
-            if (bumCount == 1)
-            {
-                cash = 1;
-                randomMob = Random.Range(0, 5);
-                GameObject selectedMob = AngryBum[randomMob];
-                GameObject newMob = Instantiate(selectedMob, chair1, transform.rotation);
-                    
-            }
-            else if (bumCount == 2)     
-            {
-                cash = 3;
-                randomMob = Random.Range(0, 5);
-                GameObject selectedMob = AngryBum[randomMob];
-                GameObject newMob = Instantiate(selectedMob, chair1, transform.rotation);
-                randomMob = Random.Range(0, 5);
-                GameObject selectedMob2 = AngryBum[randomMob];
-                GameObject newMob2 = Instantiate(selectedMob2, chair2, transform.rotation);                    
-            }
-            else if(bumCount >= 3)
-            {
-                cash = 5;
-                randomMob = Random.Range(0, 5);
-                GameObject selectedMob = AngryBum[randomMob];
-                GameObject newMob = Instantiate(selectedMob, chair1, transform.rotation);
-                randomMob = Random.Range(0, 5);
-                GameObject selectedMob2 = AngryBum[randomMob];
-                GameObject newMob2 = Instantiate(selectedMob2, chair2, transform.rotation);
-                randomMob = Random.Range(0, 5);
-                GameObject selectedMob3 = AngryBum[randomMob];
-                GameObject newMob3 = Instantiate(selectedMob3, chair3, transform.rotation);                    
-            }            
-            
+            cash = ((bumCount * bumCount) -(bumCount - 1));
             health.collectedCash = health.collectedCash + cash;
             Debug.Log("CoinCollected");
-            CoinCounterScript.CollectCoin(cash);            
-            bumCount = 0;
-            cash = 0;
+            CoinCounterScript.CollectCoin(cash);
 
+            for (int i = 0; i < bumCount; i++)
+            {
+                randomMob = Random.Range(0, 5);
+                GameObject selectedMob = AngryBum[randomMob];
+                GameObject newMob = Instantiate(selectedMob, this.transform.GetChild(i).transform.position, transform.rotation);                
+            }                        
+            bumCount = 0;                 
         }
-
-    }   
-
+    }       
 }
